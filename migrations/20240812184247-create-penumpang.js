bbe880e216c7c3fdee9896f69c7e23a59fd4cb1a
mutawakkilalallah <mutawakkilalallah.19@gmail.com>
2024-08-18 19:51:38 +0700

    fix table name on migrate

--- migrations/20240812184247-create-penumpang.js
+++ migrations/20240812184247-create-penumpang.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("Penumpangs", {
+    await queryInterface.createTable("penumpangs", {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -52,6 +52,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable("Penumpangs");
+    await queryInterface.dropTable("penumpangs");
   },
 };
