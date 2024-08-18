fb7ad577e6bc3242aa847cd99b42570741626ef4
mutawakkilalallah <mutawakkilalallah.19@gmail.com>
2024-08-18 20:01:58 +0700

    fix table dropspot name

--- migrations/20240727142727-create-dropspot.js
+++ migrations/20240727142727-create-dropspot.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("Dropspots", {
+    await queryInterface.createTable("dropspots", {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -45,6 +45,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable("Dropspots");
+    await queryInterface.dropTable("dropspots");
   },
 };
