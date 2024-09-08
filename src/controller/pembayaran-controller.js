71bda271d047919934c5a769eb1428ea0fdf97ca
mutawakkilalallah <mutawakkilalallah.19@gmail.com>
2024-09-08 15:32:35 +0700

    fix error upload

--- src/controller/pembayaran-controller.js
+++ src/controller/pembayaran-controller.js
@@ -244,13 +244,13 @@ module.exports = {
             const I = row.getCell("I").value;
             const K = row.getCell("K").value;
             // Pastikan nilai tidak kosong sebelum menambahkannya ke array
-            // if (I === "Lunas") {
-            data.push({
-              niup: B,
-              tagihan: H != null ? H - 1000 : 0,
-              total_bayar: K != null ? H - 1000 : 0,
-            });
-            // }
+            if (I === "Lunas") {
+              data.push({
+                niup: B,
+                tagihan: H != null ? H - 1000 : 0,
+                total_bayar: K != null ? K - 1000 : 0,
+              });
+            }
           }
         });
         const promises = data.map(async (d) => {
