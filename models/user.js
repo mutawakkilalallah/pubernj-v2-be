f56eb84b7cfc5f086a764a33853e2d4b5d984fcd
mutawakkilalallah <mutawakkilalallah.19@gmail.com>
2024-09-11 19:56:18 +0700

    fix allow null true user model

--- models/user.js
+++ models/user.js
@@ -57,7 +57,7 @@ module.exports = (sequelize, DataTypes) => {
         type: DataTypes.STRING,
       },
       hp: {
-        allowNull: false,
+        allowNull: true,
         type: DataTypes.STRING,
       },
       role: {
